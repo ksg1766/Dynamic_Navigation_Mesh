469b7423a813bf1c928c1683c67b2314a1f74ba9
abyss\ksg1766 <ksg7186@gmail.com>
2024-08-19 18:02:50 +0900

    develop : 고정 소숫점 라이브러리를 추가했지만... 바꿀게 너무 많아 딱히 쓰진 않을 것 같다.

--- Reference/Headers/Engine_Defines.h
+++ Reference/Headers/Engine_Defines.h
@@ -21,6 +21,9 @@
 #include <DirectXTK/DDSTextureLoader.h>
 #include <DirectXTK/WICTextureLoader.h>
 #include <Triangle/triangle.h>
+#include <fpm/fixed.hpp>
+#include <fpm/math.hpp>
+#include <fpm/ios.hpp>
 
 //#include <FMOD/fmod.h>
 //#include <FMOD/fmod.hpp>
